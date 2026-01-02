7acc806f993d02225a99f69ec6d3666076898d82
nivco360 <63196956+nivco360@users.noreply.github.com>
2026-01-02 12:55:43 +0200

    update experiment to current state

--- src/experiment.py
+++ src/experiment.py
@@ -9,9 +9,9 @@ def main():
 
     #C:\Users\owner\Desktop\Niv\Motor imagery skill\cache
 
-    # vars = load_cached_variables(load_path='C:\Users\owner\Desktop\Niv\Motor imagery skill\cache\sub_201')
-    vars = load_cached_variables(load_path='C:/Users/owner/Desktop/Niv/Motor imagery skill/cache/sub_205')
-    # vars = load_cached_variables(load_path='C:\Users\owner\Desktop\Niv\Motor imagery skill\cache\sub_206')
+    vars = load_cached_variables(load_path='C:/Users/owner/Desktop/Niv/Motor imagery skill/cache/sub_201')
+    # vars = load_cached_variables(load_path='C:/Users/owner/Desktop/Niv/Motor imagery skill/cache/sub_205')
+    # vars = load_cached_variables(load_path='C:/Users/owner/Desktop/Niv/Motor imagery skill/cache/sub_206')
 
 
     globals().update(vars)
@@ -21,15 +21,15 @@ def main():
 
     # C:\Users\owner\Desktop\Niv\Niv_github\Figures
 
-    # save_dir = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201"
-    # save_dir_10_day_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/10_day_window"
-    # save_dir_centers = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/centers"
-    # save_dir_trial_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/trial_window"
+    save_dir = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201"
+    save_dir_10_day_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/10_day_window"
+    save_dir_centers = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/centers"
+    save_dir_trial_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_201/trial_window"
 
-    save_dir = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205"
-    save_dir_10_day_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/10_day_window"
-    save_dir_centers = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/centers"
-    save_dir_trial_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/trial_window"
+    # save_dir = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205"
+    # save_dir_10_day_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/10_day_window"
+    # save_dir_centers = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/centers"
+    # save_dir_trial_window = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_205/trial_window"
 
 
     # save_dir = r"C:/Users/owner/Desktop/Niv/Niv_github/Figures/sub_206"
@@ -57,43 +57,85 @@ def main():
 
 
     # ─── 3) Cluster‐separation plots ────────────────────────────────────────────
-    # visualizations.plot_auc_vs_cluster_separation(
+    visualizations.plot_auc_vs_cluster_separation(
+          X_csp_features_scaled,X_csp_features_scaled_2d,
+          y_label, days_label, clf_loaded,
+          start_test_day,end_test_day, dim=2,directory=save_dir)
+
+    visualizations.plot_auc_vs_cluster_separation(
+        X_csp_features_scaled, X_pca_features_2D,
+        y_label, days_label, clf_loaded,
+        start_test_day, end_test_day,
+        dim=2, reducer='PCA',directory=save_dir
+    )
+
+    visualizations.plot_auc_vs_cluster_separation(
+        X_csp_features_scaled,X_umap_features_2D,
+        y_label, days_label, clf_loaded,
+        start_test_day,end_test_day, 
+        dim=2, reducer='UMAP',directory=save_dir
+    )
+
+    visualizations.plot_auc_vs_cluster_separation(
+        X_csp_features_scaled,X_pca_features_3D,
+        y_label, days_label, clf_loaded,
+        start_test_day,end_test_day,
+        dim=3, reducer='PCA',directory=save_dir
+    )
+
+    visualizations.plot_auc_vs_cluster_separation(
+        X_csp_features_scaled,X_umap_features_3D,
+        y_label, days_label, clf_loaded,
+        start_test_day,end_test_day,
+        dim=3, reducer='UMAP',directory=save_dir
+    )
+
+    visualizations.plot_auc_vs_cluster_separation(
+        X_csp_features_scaled,X_csp_features_scaled,
+        y_label, days_label, clf_loaded,
+        start_test_day,end_test_day, 
+        directory=save_dir
+    )
+
+    # ─── 4) Variance‐vs‐accuracy smoothing ──────────────────────────────────────
+
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #       X_csp_features_scaled,X_csp_features_scaled_2d,
     #       y_label, days_label, clf_loaded,
-    #       start_test_day,end_test_day,dim=2,directory=save_dir)
+    #       start_test_day,end_test_day, dim=2,directory=save_dir)
 
-    # visualizations.plot_auc_vs_cluster_separation(
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #     X_csp_features_scaled, X_pca_features_2D,
     #     y_label, days_label, clf_loaded,
     #     start_test_day, end_test_day,
     #     dim=2, reducer='PCA',directory=save_dir
     # )
 
-    # visualizations.plot_auc_vs_cluster_separation(
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #     X_csp_features_scaled,X_umap_features_2D,
     #     y_label, days_label, clf_loaded,
-    #     start_test_day,end_test_day,
+    #     start_test_day,end_test_day, 
     #     dim=2, reducer='UMAP',directory=save_dir
     # )
 
-    # visualizations.plot_auc_vs_cluster_separation(
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #     X_csp_features_scaled,X_pca_features_3D,
     #     y_label, days_label, clf_loaded,
     #     start_test_day,end_test_day,
     #     dim=3, reducer='PCA',directory=save_dir
     # )
 
-    # visualizations.plot_auc_vs_cluster_separation(
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #     X_csp_features_scaled,X_umap_features_3D,
     #     y_label, days_label, clf_loaded,
     #     start_test_day,end_test_day,
     #     dim=3, reducer='UMAP',directory=save_dir
     # )
 
-    # visualizations.plot_auc_vs_cluster_separation(
+    # visualizations.plot_auc_vs_cluster_separation_smoothed(
     #     X_csp_features_scaled,X_csp_features_scaled,
     #     y_label, days_label, clf_loaded,
-    #     start_test_day,end_test_day,
+    #     start_test_day,end_test_day, 
     #     directory=save_dir
     # )
 
@@ -117,7 +159,8 @@ def main():
 
     # # ─── 5) Delta‐matrix analyses ─────────────────────────
 
-    # delta_auc_matrix, delta_inter_var_matrix, delta_intra_var_matrix_idle, delta_intra_var_matrix_motor = visualizations.delta_auc_var(X_csp_features_scaled,y_label,days_label,clf_loaded,start_test_day,end_test_day,directory=save_dir) 
+    delta_auc_matrix, delta_inter_var_matrix, delta_intra_var_matrix_idle, delta_intra_var_matrix_motor = visualizations.delta_auc_var(X_csp_features_scaled,y_label,days_label,clf_loaded,start_test_day,end_test_day, smooth=False, directory=save_dir) 
+    visualizations.delta_vs_raw(X_csp_features_scaled,y_label,days_label,clf_loaded,start_test_day,end_test_day, smooth=False, directory=save_dir) 
 
     # # ─── 6) 10-day window projection
     # SUB201
@@ -130,22 +173,22 @@ def main():
     #     start_day=1,window_size=10,overlap_size=5)
     
     # #SUB205
-    visualizations.plot_sliding_windows(
-        days_label,y_label, clf_loaded,
-        X_csp_features_scaled, X_csp_features_scaled_2d,
-        X_pca_features_2D,X_umap_features_2D,
-        X_pca_features_3D,X_umap_features_3D,
-        save_dir_win=save_dir_10_day_window,
-        start_day=1,window_size=5,overlap_size=0)
-
-    # #SUB206
     # visualizations.plot_sliding_windows(
     #     days_label,y_label, clf_loaded,
     #     X_csp_features_scaled, X_csp_features_scaled_2d,
     #     X_pca_features_2D,X_umap_features_2D,
     #     X_pca_features_3D,X_umap_features_3D,
     #     save_dir_win=save_dir_10_day_window,
-    #     start_day=1,window_size=6,overlap_size=0)
+    #     start_day=1,window_size=5,overlap_size=0)
+
+    # #SUB206
+    visualizations.plot_sliding_windows(
+        days_label,y_label, clf_loaded,
+        X_csp_features_scaled, X_csp_features_scaled_2d,
+        X_pca_features_2D,X_umap_features_2D,
+        X_pca_features_3D,X_umap_features_3D,
+        save_dir_win=save_dir_10_day_window,
+        start_day=1,window_size=6,overlap_size=0)
 
 
 
